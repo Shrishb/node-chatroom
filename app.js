b2a1fe639e40021b6a6b4368f2a567ae597acad6
Shrish <b.shrish12@gmail.com>
2020-03-28 13:35:30 +1100

    setup socketio conections

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection', socket => {
+console.log("New Connection");
+
+//new client
+socket.emit("welcome");
+
+//rest of the clients except new client
+socket.broadcast.emit('message', 'New user joined');
+
+// all the clients
+io.emit();
+});
 
 const PORT = process.env.port || 3000;
 
-app.listen(PORT, () => console.log("Server started"));
+server.listen(PORT, () => console.log("Server started"));
