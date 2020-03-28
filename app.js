ba528a608538a73e747890f0907896d985434bb6
Shrish <b.shrish12@gmail.com>
2020-03-28 15:33:40 +1100

    show new messages

--- app.js
+++ app.js
@@ -4,24 +4,29 @@ const socketio = require("socket.io");
 const path = require("path");
 const app = express();
 
+const formatMessage = require("./util/formatMessage");
+
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-io.on('connection', socket => {
-console.log("New Connection");
+io.on("connection", socket => {
+  console.log("New Connection");
 
+  //rest of the clients except new client
+  socket.broadcast.emit("message",formatMessage('admin', "New user joined"));
 
+  // all the clients
+  io.emit();
 
-//new client
-socket.emit("welcome");
+  socket.on("disconnect", function() {
+    io.emit("user disconnected");
+  });
 
-//rest of the clients except new client
-socket.broadcast.emit('message', 'New user joined');
-
-// all the clients
-io.emit();
+  socket.on("sendMessage", function(message) {
+    console.log("user typed", message);
+  });
 });
 
 const PORT = process.env.port || 3000;
--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,28 @@
 const socket = io();
+const sendMessage = document.getElementById("chat-form");
+const messages = document.querySelector(".chat-messages");
 
-socket.on('message', message => {
-    console.log(message);
+socket.on("message", message => {
+    messages.scrollTop = messages.scrollHeight;
 });
+
+sendMessage.addEventListener("submit", e => {
+    e.preventDefault();
+
+    let msg = e.target.elements.msg.value;
+
+    socket.emit('sendMessage', msg);
+
+    e.target.elements.msg.value ="";
+    e.target.elements.msg.focus();
+  });
+
+  function displayMessage(message){
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `<p class="meta">${message.name} <span>${message.time}</span></p>
+    <p class="text">
+      ${message.text}
+    </p>`;
+    document.querySelector('.chat-messages').appendChild(div);
+  }
